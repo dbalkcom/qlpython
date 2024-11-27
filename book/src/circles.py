b91111f5bf2a4fcd8ac2cf68799b4592845b70c1
Devin Balkcom <devin.balkcom@dartmouth.edu>
2024-11-27 16:38:44 -0500

    typo fix

--- book/src/circles.py
+++ book/src/circles.py
@@ -8,6 +8,5 @@ init_graphics(400, 400)  # create a new game window of size 400 x 400
 circle(Color('blue'), 125, 100, 50)
 circle(Color('green'), 275, 100, 50)
  
-# copy the drawing onto the window and wait for exit 
-# button to be clicked:
+# copy the drawing onto the window and wait for exit button to be clicked:
 draw_and_wait_for_exit()  
